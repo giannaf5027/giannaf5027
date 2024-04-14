c9d6b19505f81b36230fafaa7728c16640d297d5
kondapallih <166643584+kondapallih@users.noreply.github.com>
2024-04-13 20:01:48 -0400

    Added decoded function in the proper order

--- Encoder.py
+++ Encoder.py
@@ -1,20 +1,4 @@
   
-   def decode(s):           
-    r = ""               
-    for n in s:          
-        n = int(n)       
-        n -= 3           
-        if n == -3:      
-            r += "0"     
-        elif n == -2:    
-            r += "9"     
-        elif n == -1:    
-            r += "8"     
-        else:            
-            r += str(n)  
-        return r       
-
-
 def encode(s):
     r = ""
     for n in s:
@@ -29,6 +13,24 @@ def encode(s):
         else:
             r += str(n)
     return r
+
+
+  
+   def decode(s):           
+    r = ""               
+    for n in s:          
+        n = int(n)       
+        n -= 3           
+        if n == -3:      
+            r += "0"     
+        elif n == -2:    
+            r += "9"     
+        elif n == -1:    
+            r += "8"     
+        else:            
+            r += str(n)  
+        return r     
+      
 
 if __name__ == '__main__':
     e = ""
